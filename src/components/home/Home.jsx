ce7ea01da31b73eed96635d31f7e9930e6f129ec
maskaa22 <kyrosika565@gmail.com>
2025-03-02 22:16:13 +0200

    add home h1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,9 @@
+import s from "./Home.module.css";
+import { FaUserAlt } from "react-icons/fa";
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={s.home}>
+      <h1>This home page for users</h1> <FaUserAlt className={s.user}/>
+    </div>
+  );
 };
